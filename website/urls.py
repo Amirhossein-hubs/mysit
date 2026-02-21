c4118d41c5468e83ea3dcb9117f79c8f3d9fd31d
Amirhossein-hubs <amirhoseinkeahany@gmail.com>
2026-02-21 06:17:01 -0800

    creat app blog and conction html
    creat app blog and conction html

--- website/urls.py
+++ website/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import about_view, contact_view, website
+from .views import about_view, contact_view, website, element_view, test_view
+
+app_name = 'website'
 
 urlpatterns = [
-    path('about', about_view),
-    path('contact', contact_view),
-    path('', website)
+    path('about', about_view, name='about'),
+    path('contact', contact_view, name='contact'),
+    path('elements', element_view, name='elements'),
+    path('test', test_view, name='test'),
+    path('', website, name='index'),
 ]
