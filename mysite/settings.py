6beb83c42460e517ae683d6431c7740f2c4f21e2
Amirhossein-hubs <amirhoseinkeahany@gmail.com>
2026-09-17 05:43:57 -0700

    Fix database config, ALLOWED_HOSTS, and DEBUG for production

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,9 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-id1ha8wtwfhae-i57=lb7pi1a-&(1w+9u*m8ek13)si841&-7d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mysit-bn6t.onrender.com']
+ 
 
 
 
@@ -143,11 +144,18 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 #     )
 # }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True,
+    )
 }
 
 
